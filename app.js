e96c1f90ff3534f63b38366f88d9bf11a6dca87f
Afaf Afridi <ibrahim.hasan00710@gmail.com>
2025-07-05 22:39:30 +0500

    rizzler commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let number=prompt("Enter a number to check if it is negative,postive or zero");
+if(number>0){
+    console.log("The number is positive");
+    alert("The number is positive");
+}else if(number<0){
+    console.log("The number is negative");
+    alert("The number is negative");
+}else{
+    console.log("The number equals to zero");
+    alert("The number equals to zero");
+}
